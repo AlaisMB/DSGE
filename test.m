b208bc58dc345ed1ab973a0064a271fd2b8655de
SuperMayo <a.mayerowitz@gmail.com>
2016-12-06 19:29:16 +0100

    minor fix (add semicolons)

--- test.m
+++ test.m
@@ -27,8 +27,8 @@ fprintf('The model will be simulated for %s\n ',country);
 % Calibration and steady state
 %**********************
 
-C_us = [ 0.95 ; 0.95 ; 0.009  ; 0.008  ;  1.014 ; 0.377 ; 0.009   ; 0.2  ; 0.3   ;  0.025 ; - 1/3 ; 1/9 ; 0.197 ; 0.1  ; 1 ; 0.01  ; 0.988]
-C_fr = [ 0.98 ; 0.99 ;  0.009 ; 0.0058 ; 1.028  ;  0.50 ; 0.0098  ; 0.2  ; 0.335 ; 0.0125 ; - 1/3 ; 1/9 ; 0.225 ; 0.25 ; 1 ; 0.007 ; 0.988]
+C_us = [ 0.95 ; 0.95 ; 0.009  ; 0.008  ;  1.014 ; 0.377 ; 0.009   ; 0.2  ; 0.3   ;  0.025 ; - 1/3 ; 1/9 ; 0.197 ; 0.1  ; 1 ; 0.01  ; 0.988];
+C_fr = [ 0.98 ; 0.99 ;  0.009 ; 0.0058 ; 1.028  ;  0.50 ; 0.0098  ; 0.2  ; 0.335 ; 0.0125 ; - 1/3 ; 1/9 ; 0.225 ; 0.25 ; 1 ; 0.007 ; 0.988];
 
 
 if cntryid == 1 
@@ -37,23 +37,23 @@ else
 	 C = [C_us]; 
 end	
 
-rho_an = C(1)
-rho_ap = C(2)
-epsilon_an = C(3)
-epsilon_ap = C(4)
-g_barre = C(5)
-rho_g = C(6)
-epsilon_g = C(7)
-H = C(8)
-alpha = C(9)
-delta = C(10)	
-a = C(11)
-sigma = C(12)
-gamma = C(13)
-MPY = C(14) 
-phi_us = C(15) 
-MC = C(16)
-beta = C(17)
+rho_an = C(1);
+rho_ap = C(2);
+epsilon_an = C(3);
+epsilon_ap = C(4);
+g_barre = C(5);
+rho_g = C(6);
+epsilon_g = C(7);
+H = C(8);
+alpha = C(9);
+delta = C(10);	
+a = C(11);
+sigma = C(12);
+gamma = C(13);
+MPY = C(14);
+phi_us = C(15); 
+MC = C(16);
+beta = C(17);
 
 A_barre = 0.2;
 P = 0.3; 
