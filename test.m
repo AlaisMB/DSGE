6f5001c2a803849f136a3a08a58e8426a1c1c7dc
SuperMayo <a.mayerowitz@gmail.com>
2016-12-14 16:45:19 +0100

    working model

--- test.m
+++ test.m
@@ -10,7 +10,7 @@ close all
 %                                                             %
 % ------------------------------------------------------------%
 %                     Replication Code by                     %
-%    Ala?s Martin-Baillon, Antoine Mayerowitz, Arthur Stalla  %
+%    Alais Martin-Baillon, Antoine Mayerowitz, Arthur Stalla  %
 %                          Fall 2016                          %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
@@ -67,27 +67,25 @@ end
 [rho_an rho_ap epsilon_an epsilon_ap g_barre rho_g epsilon_g H alpha delta a sigma gamma MPY phi MC beta zeta] = calib{:};
 
 
-rho_a = rho_an; % We suppose this for the moment. We should let the user decide.
-A_barre = 1;
-P = 0.3; 
+rho_a = rho_ap; % We suppose this for the moment. We should let the user decide.
+A_barre = 1; 
 
 z = (1/beta) - 1 + delta; 
 
 KY = (alpha / (1 + gamma )) * (1/z) ; 
 CY = 1 - delta * KY  ; 
-YH =(A_barre)^(1/(1-sigma)) *  KY^(alpha/(1-alpha))  ;
 
-Y = YH * H;
+Y = KY^(alpha/(1-alpha))*H;
 C = CY* Y ;
 K = KY * Y ; 
 
-w = ((1-alpha)/ (1+gamma))* YH ;
-gamma_2 = - (1- g_barre * (1+z - delta)) * (MPY * (1/CY))^(1/sigma) ;
-vP = gamma / (1 + gamma) ; 
-v = vP * P ;
+w = ((1-alpha)/ (1+gamma))* (Y/H) ;
+gamma_2 = - ( 1 - g_barre * (1 + z - delta) ) * ( MPY / CY )^( 1/sigma ) ;
+vP = gamma / (1 + gamma) ;  
 mu = gamma;
 
 MP = MPY * Y ;
+MPY=0.25;
 
 % Change in variables
 Avar = (1+ sigma * ( a - 1)) / sigma;
@@ -96,17 +94,21 @@ Cvar = (1 + sigma * ( a - 1))/ (sigma * (C^((sigma-1)/sigma) + gamma_2*(MP^((sig
 Dvar = (g_barre / beta ) - 1;
 Evar = (K*(delta - z) - w * H + C)^(-1);
 
+epsilon=1-(g_barre/beta);
+B1=((a*sigma-sigma+1)/sigma)*(1/(1-(epsilon*MPY*CY)));
+B2=((a*sigma-sigma+1)/sigma)*(1/(1-(CY/(epsilon*MPY))));
+
 
 %%%%%%%%%%%% Display parameters value %%%%%%%%%%%%
 
 disp(' ');
 param1=[alpha beta delta H rho_ap rho_g gamma sigma];
-param2=[Y     C     K     w   v   z   MP ];
+param2=[Y     C     K     w      z   MP ];
 disp('    alpha     beta  	delta 	   H   	   rho_a    rhog     gamma      sigma'); 
 disp(' ');
 disp(param1);
 disp(' ');
-disp('    Y         C   	K  	  w         v  	      z	       MP ');
+disp('    Y         C   	K  	  w       z	       MP ');
 disp(param2);
 
 
@@ -114,8 +116,6 @@ disp(param2);
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%		   	Linearized Model in Matrix Form 				 %%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-
 M1=zeros(7,7); 
 M2=zeros(7,5);
 M3=zeros(7,2);
@@ -130,41 +130,38 @@ M6L = zeros(5,2);
 
 
 %%%%%%%%%%%% STATIC EQUATIONS %%%%%%%%%%%%
-
-% Equation 23
+% Equation 23 -> OK
 M1(1,1) = (Avar/(1-Bvar)) - 1/sigma;
 M1(1,7) = (Avar*Bvar)/(1-Bvar);
 
-M2(1,2) = - (Avar*Bvar)/(1-Bvar);
+M2(1,2) = (Avar*Bvar)/(1-Bvar);
 M2(1,5) = 1;
 
-% Equation 24
+% Equation 24 -> OK
 M1(2,2) = (zeta * H)/(1-H);
 M1(2,3) = 1;
 
 M2(2,5) = -1;
 
+%Equation 40 -> OK
+M1(3,7) = 1;
 
-% Equation 25
-M1(3,1) = -(Dvar*Cvar*C^((sigma-1)/sigma));
-M1(3,4) = g_barre*z;
-M1(3,7) = (Dvar*((sigma-1)/sigma) + 1 - Cvar);
+M2(3,:) = [0 1 -1 0 0];
 
-M2(3,2) = (-1/sigma)*Dvar + Cvar;
-M2(3,5) = -Dvar;
+M3(3,2) = 1;
 
-% Equation 30
-M1(4,:) = [0 -1 -1 0 1 0 0];
+% Equation 30 -> OK
+M1(4,:) = [0 1 1 0 -1 0 0];
 
-M2(4,4) = mu/(1+ mu);
+M2(4,4) = -mu/(1+ mu);
 
-% Equation 31
-M1(5,:) = [0 0 0 -1 1 0 0];
+% Equation 31 -> OK
+M1(5,:) = [0 0 0 1 -1 0 0];
 
-M2(5,1) = 1;
-M2(5,4) = mu/(1+mu);
+M2(5,1) = -1;
+M2(5,4) = - mu/(1+mu);
 
-% Equation 34
+% Equation 34 -> OK
 M1(6,2) = alpha - 1;
 M1(6,5) = 1;
 
@@ -172,45 +169,56 @@ M2(6,1) = alpha;
 
 M3(6,1) = 1;
 
-% Equation 35
+% % Equation 35 !!! 404 not Found !!! (equation (6) ?)
 M1(7,2) = H*w;
 M1(7,3) = H*w;
 M1(7,4) = z*K;
 M1(7,5) = -Y;
-M1(7,6) = -w*H - z*K;
+M1(7,6) =Y -w*H - z*K;
 
 M2(7,1) = -z*K;
 
+
 %%%%%%%%%%%% DYNAMICS EQUATIONS %%%%%%%%%%%%
 
-% Equation 26
+% Equation 26 -> OK
 M4I(1,5) = 1;
 M4L(1,5) = -1;
 
-M5I(1,4) = -(1-beta*(1-delta));
+M5I(1,4) = -1 - beta * ( delta - 1);
 
-% Equation 27
-M4I(2,1) = Evar * K;
-M4L(2,1) = Evar*K*(delta-1-z);
+% Equation 27   -> Fausse
+% M4I(2,1) = Evar * K;
+% M4L(2,1) = Evar*K*(delta-1-z);
+% 
+% M5L(2,:) = [-Evar Evar*w*H Evar*w*H Evar*z*K 0 1 0];
 
-M5L(2,:) = [-Evar Evar*w*H Evar*w*H Evar*z*K 0 1 0];
+M4I(2,1)=KY;
+M4L(2,1)=-(1/beta)*KY;
+M5L(2,1)=-CY;
+M5L(2,2)= (1-alpha)/(1+gamma);
+M5L(2,3)= (1-alpha)/(1+gamma);
+M5L(2,4)= alpha/(1+gamma);
+M5L(2,6)= gamma/(1+gamma);
 
-% Equation 32
+% Equation 32   -> OK
 M4L(3,4) = Y/(1+mu);
 
 M5I(3,7) = phi*beta*g_barre^3;
 M5L(3,7) = -phi*g_barre^2;
 
-% Equation 40
+%Equation 25 
+M5I(5,1) = -( Dvar*Cvar*C^( (sigma-1)/sigma ) );
+M5I(5,4) = g_barre*z;
+M5I(5,7) = (Dvar + 1 +Dvar*( (-1/sigma) + gamma_2 * Cvar*(MP)^((sigma-1)/sigma)) );
+
+M4I(5,2) = Dvar* ( (-1/sigma) + gamma_2 * Cvar*(MP)^((sigma-1)/sigma) );
+M4I(5,5) = -Dvar;
+
+
+% Equation 40 bis magique -> OK
 M4I(4,2) = 1;
 M4L(4,3) = -1;
-
-% Equation 40 bis magique
-M4I(5,3) = -1;
-M4L(5,3) = 1;
-
-M5I(5,7) = 1;
-M6I(5,2) = -1;
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%                      Reduced Form                 	     %%
@@ -264,14 +272,28 @@ P1 = inv(P);
 RHO = [rho_a 0 ; 0 rho_g];
 
 % We decompose P1 into 3 matrices in order to decompose predetermined and jump
-% variables and we'll get P1 = [Pss Psa ; Pas Paa]
+% variables and we'll get
+%             ?             ?
+%             |  Pss   Psa  |
+%             | (1x2) (1x2) |
+%   inv(P)=   |             | 
+%             |  Pas   Paa  |
+%             | (1x2) (1x2) |
+%             ?             ?
 Pss = P1(1:2,1:2);
 Psa = P1(1:2,3:5);
 Pas = P1(3:5,1:2);
 Paa = P1(3:5,3:5);
 
 % We split W into 2 systems of equation, W1 that depends on predetermined
-% and W2 that depends on jump such that W = [W1;W2]
+% and W2 that depends on jump such that
+%             ?             ?
+%             |  Pss   Psa  |
+%             | (1x2) (1x2) |
+%   W=        |             | 
+%             |  Pas   Paa  |
+%             | (1x2) (1x2) |
+%             ?             ?
 W1 = W(1:2,:);
 W2 = W(3:5,:);
 
@@ -349,37 +371,106 @@ z = Rd(4,2:(nrep+1))';
 Y = Rd(5,1:nrep)';
 pi = Rd(6,1:nrep)';
 f = Rd(7,1:nrep)';
+mu = Rd(8,1:nrep)';
+I = (Y-CY*C)/(1-CY);
 
 figure
 plot(1:nrep,[w,C,z],'-s');
 legend('w','C','r','Location','southoutside');
-title('French calibration - 1% shock on g')
 xlabel('Quarters');
 ylabel('relative deviation (%)');
 set(gcf, 'Color', [1,1,1]);
 hline = refline([0 0]);
 set(hline,'LineStyle','--','color','black');
+
+figure
+plot(1:nrep,[pi,mu],'-s');
+legend('pi','mu','Location','southoutside');
+xlabel('Quarters');
+ylabel('relative deviation (%)');
+set(gcf, 'Color', [1,1,1]);
+
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% Stochastic Simulation
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% 
 
-nsimul = 5
+nsimul = 100;
 
-nlong=10;
+nlong=100;
 
-% Drawing shocks
+% Drawing innovations
 aleaa = normrnd(0,epsilon_ap,nlong,nsimul);
 aleag = normrnd(0,epsilon_g,nlong,nsimul);
 
 % Drawing autoregressive shocks
 for i=2:nlong
-    epsa(i,:) = rho_ap*aleaa(i-1,:) + aleaa(i);
+    epsa(i,:) = rho_a*aleaa(i-1,:) + aleaa(i);
     epsg(i,:) = rho_g*aleag(i-1,:) + aleag(i);
 end
-for j=2:nsimul
-    eps = [epsa(:,j) epsg(:,j);
-    MS(:,1)=[0;0];
-    for i=1:nlong
-        MS(:,1) = M(1:2,1:2)*MS(:,i-1)+M(1:2,3)*eps(i,1)+M(1:2,4)*eps(i,2);
+
+% We can now simulate the model
+h = waitbar(0,'Simulations are running, please wait...');
+for j=1:nsimul
+    waitbar(j / nsimul)
+    
+    S(:,1)=[0;0]; % State variables initialization
+    for i=2:nlong;
+        S(:,i)=M(1:2,1:2)*S(:,i-1)+M(1:2,3)*epsa(i-1,j)+M(1:2,4)*epsg(i-1,j); % We compute the dynamics of the state variables
+    end;
+    
+    for i=1:nlong;
+        CC(i,j) = PID(1,1:2)*S(:,i)+PID(1,3)*epsa(i,j)+PID(1,4)*epsg(i,j);
+        HC(i,j) = PID(2,1:2)*S(:,i)+PID(2,3)*epsa(i,j)+PID(2,4)*epsg(i,j);
+        WC(i,j) = PID(3,1:2)*S(:,i)+PID(3,3)*epsa(i,j)+PID(3,4)*epsg(i,j);
+        RC(i,j) = PID(4,1:2)*S(:,i)+PID(4,3)*epsa(i,j)+PID(4,4)*epsg(i,j);
+        YC(i,j) = PID(5,1:2)*S(:,i)+PID(5,3)*epsa(i,j)+PID(5,4)*epsg(i,j);
+        PC(i,j) = PID(6,1:2)*S(:,i)+PID(6,3)*epsa(i,j)+PID(6,4)*epsg(i,j);
+        FC(i,j) = PID(7,1:2)*S(:,i)+PID(7,3)*epsa(i,j)+PID(7,4)*epsg(i,j);
     end
 end
+close(h)
+
+
+% Plot the first simulation
+
+figure
+subplot(221),plot(CC(:,1));
+title('Consumption')
+xlabel('Quarters')
+ylabel('% Dev.')
+
+subplot(222),plot(YC(:,1));
+title('Output')
+xlabel('Quarters')
+ylabel('% Dev.')
+
+subplot(223),plot(WC(:,1));
+title('Wages')
+xlabel('Quarters')
+ylabel('% Dev.')
+
+subplot(224),plot(HC(:,1));
+title('Hours')
+xlabel('Quarters')
+ylabel('% Dev.')
+
+figure
+subplot(221),plot(aleaa(:,1));
+title('White noise shocks, Technology')
+xlabel('Quarters')
+ylabel('% Dev.')
+
+subplot(222),plot(aleag(:,1));
+title('White noise shocks, Money growth')
+xlabel('Quarters')
+ylabel('% Dev.')
+
+subplot(223),plot(epsa(:,1));
+title('Total factor productivity')
+xlabel('Quarters')
+ylabel('% Dev.')
+
+subplot(224),plot(epsg(:,1));
+title('Money growth rate')
+xlabel('Quarters')
+ylabel('% Dev.')
