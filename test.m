b676ff6dd159df40f7407a91bb2e9c71fc9bf2a8
SuperMayo <a.mayerowitz@gmail.com>
2016-12-14 10:36:18 +0100

    small fix

--- test.m
+++ test.m
@@ -2,11 +2,20 @@ clear
 close all
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%		The business cycle, J.-O. Hairault, F. Portier 	%
+%              Money, New-Keynesian macroeconomics            %
+%                    and the business cycle                   %
+%                                                             %
+%                  J.-O. Hairault, F. Portier                 %
+%               European Economic Review, 1993                %
+%                                                             %
+% ------------------------------------------------------------%
+%                     Replication Code by                     %
+%    Ala?s Martin-Baillon, Antoine Mayerowitz, Arthur Stalla  %
+%                          Fall 2016                          %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%%%%%%%%%%%%%%%%%%%%%%  Choix du Pays %%%%%%%%%%%%%%%%%%%%%%%%
+%%                     Country Choice                        %%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 cntrymap = containers.Map([1,2],{'France','USA'}); % Map the selected number with country name (To display country name later)
@@ -24,17 +33,34 @@ country = cntrymap(cntryid); % Stock the country name
 fprintf('The model will be simulated for %s\n ',country);
 
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%Calibration and steady state%%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%              Calibration and steady state                 %%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-C_us = num2cell([ 0.95 ; 0.95 ; 0.009  ; 0.008  ;  1.014 ; 0.377 ; 0.009   ; 0.2  ; 0.3   ;  0.025 ; - 1/3 ; 1/9 ; 0.197 ; 0.1  ; 1 ; 0.01  ; 0.988; 0]);
-C_fr = num2cell([ 0.98 ; 0.99 ;  0.009 ; 0.0058 ; 1.028  ;  0.50 ; 0.0098  ; 0.2  ; 0.335 ; 0.0125 ; - 1/3 ; 1/9 ; 0.225 ; 0.25 ; 1 ; 0.007 ; 0.988; 0]);
+%      |  US  | |  FR  |
+param=[ 0.95     0.98  ;    % rho_an
+        0.95     0.99  ;    % rho_ap
+        0.009    0.009 ;    % epsilon_an
+        0.008    0.0058;    % epsilon_ap
+        1.014    1.028 ;    % g_barre
+        0.377    0.50  ;    % rho_g
+        0.009    0.0098;    % epsilon_g
+        0.2      0.2   ;    % H
+        0.3      0.335 ;    % alpha 
+        0.025    0.0125;    % delta
+        -1/3     -1/3  ;    % a
+        1/9      1/9   ;    % sigma
+        0.197    0.225 ;    % gamma
+        0.1      0.25  ;    % MPY
+        1        1     ;    % phi
+        0.1      0.007 ;    % MC
+        0.988    0.988 ;    % beta
+        0        0     ;];  % zeta
 
 if cntryid == 1 
-	calib = [C_fr];
+	calib = num2cell(param(:,2));
 else
-	calib = [C_us]; 
+	calib = num2cell(param(:,1)); 
 end	
 
 % Declare variables
@@ -42,14 +68,14 @@ end
 
 
 rho_a = rho_an; % We suppose this for the moment. We should let the user decide.
-A_barre = 0.2;
+A_barre = 1;
 P = 0.3; 
 
 z = (1/beta) - 1 + delta; 
 
 KY = (alpha / (1 + gamma )) * (1/z) ; 
 CY = 1 - delta * KY  ; 
-YH = (A_barre)^(1/(1-sigma)) * KY^(alpha/(1-alpha)) * H ;
+YH =(A_barre)^(1/(1-sigma)) *  KY^(alpha/(1-alpha))  ;
 
 Y = YH * H;
 C = CY* Y ;
@@ -57,8 +83,6 @@ K = KY * Y ;
 
 w = ((1-alpha)/ (1+gamma))* YH ;
 gamma_2 = - (1- g_barre * (1+z - delta)) * (MPY * (1/CY))^(1/sigma) ;
-
-
 vP = gamma / (1 + gamma) ; 
 v = vP * P ;
 mu = gamma;
@@ -66,9 +90,9 @@ mu = gamma;
 MP = MPY * Y ;
 
 % Change in variables
-Avar = (1- sigma * ( a - 1)) / sigma;
-Bvar = (1 - g_barre * (1 - z - delta ))* MP/C;
-Cvar = (1 - sigma * ( a - 1))/ (sigma * (C^((sigma-1)/sigma) + gamma_2*(MP^((sigma-1)/sigma))));
+Avar = (1+ sigma * ( a - 1)) / sigma;
+Bvar = (1 - g_barre * (1 + z - delta ))* MP/C;
+Cvar = (1 + sigma * ( a - 1))/ (sigma * (C^((sigma-1)/sigma) + gamma_2*(MP^((sigma-1)/sigma))));
 Dvar = (g_barre / beta ) - 1;
 Evar = (K*(delta - z) - w * H + C)^(-1);
 
@@ -111,7 +135,7 @@ M6L = zeros(5,2);
 M1(1,1) = (Avar/(1-Bvar)) - 1/sigma;
 M1(1,7) = (Avar*Bvar)/(1-Bvar);
 
-M2(1,2) = (Avar*Bvar)/(1-Bvar);
+M2(1,2) = - (Avar*Bvar)/(1-Bvar);
 M2(1,5) = 1;
 
 % Equation 24
@@ -120,34 +144,25 @@ M1(2,3) = 1;
 
 M2(2,5) = -1;
 
-%%% Choose between 25 and 40 %%%%
 
-    % Equation 25
-    M1(3,1) = -(Dvar*Cvar*C^((sigma-1)/sigma));
-    M1(3,4) = g_barre*z;
-    M1(3,7) = (Dvar*((sigma-1)/sigma) + 1 - Cvar);
+% Equation 25
+M1(3,1) = -(Dvar*Cvar*C^((sigma-1)/sigma));
+M1(3,4) = g_barre*z;
+M1(3,7) = (Dvar*((sigma-1)/sigma) + 1 - Cvar);
 
-    M2(3,2) = (-1/sigma)*Dvar + Cvar;
-    M2(3,5) = -Dvar;
-
-    % Equation 40
-    %M1(3,7) = 1;
-
-    %M2(3,:) = [0 1 -1 0 0];
-
-    %M3(3,2) = -1;
-
+M2(3,2) = (-1/sigma)*Dvar + Cvar;
+M2(3,5) = -Dvar;
 
 % Equation 30
 M1(4,:) = [0 -1 -1 0 1 0 0];
 
-M2(4,4) = mu/(1- mu);
+M2(4,4) = mu/(1+ mu);
 
 % Equation 31
 M1(5,:) = [0 0 0 -1 1 0 0];
 
 M2(5,1) = 1;
-M2(5,4) = mu/(1-mu);
+M2(5,4) = mu/(1+mu);
 
 % Equation 34
 M1(6,2) = alpha - 1;
@@ -167,14 +182,6 @@ M1(7,6) = -w*H - z*K;
 M2(7,1) = -z*K;
 
 %%%%%%%%%%%% DYNAMICS EQUATIONS %%%%%%%%%%%%
-
-% Equation 2 (Technology process)
-%M6I(6,1) = 1;
-%M6L(6,1) = -rho_a;
-
-% Equation 8 (Money growth process)
-%M6I(7,2) = 1;
-%M6L(7,2) = -rho_g;
 
 % Equation 26
 M4I(1,5) = 1;
@@ -198,33 +205,19 @@ M5L(3,7) = -phi*g_barre^2;
 M4I(4,2) = 1;
 M4L(4,3) = -1;
 
-M5I(4,7) = 0;
-M6I(4,2) = 0;
+% Equation 40 bis magique
+M4I(5,3) = -1;
+M4L(5,3) = 1;
 
-
-%%% Choose between Magic40 and Syrian25 %%%
-
-    % Equation 40 bis magique
-    M4I(5,3) = -1;
-    M4L(5,3) = 1;
-
-    M5I(5,7) = 1;
-    M6I(5,2) = -1;
-
-    % Equation 25 la migrante
-    %M5I(5,1) = -(Dvar*Cvar*C^((sigma-1)/sigma));
-    %M5I(5,4) = g_barre*z;
-    %M5I(5,7) = (Dvar*((sigma-1)/sigma) + 1 - Cvar);
-
-    %M4I(5,2) = (-1/sigma)*Dvar + Cvar;
-    %M4I(5,5) = -Dvar;
-
+M5I(5,7) = 1;
+M6I(5,2) = -1;
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%                      	Reduced Form            	      %
+%%                      Reduced Form                 	     %%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 M40 = M4I - M5I*inv(M1)*M2;
 M41 = M4L - M5L*inv(M1)*M2;
+
 
 M50 = M6I + M5I*inv(M1)*M3;
 M51 = M6L + M5L*inv(M1)*M3;
@@ -233,7 +226,6 @@ M51 = M6L + M5L*inv(M1)*M3;
 W = -inv(M40)*M41;
 R = inv(M40)*M50;
 Q = inv(M40)*M51;
-
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%                      	Eigenvalues            			  %
@@ -259,6 +251,10 @@ pause
 disp('Eigenvalues:')
 disp(EIG);
 
+if back == 2 && 5-(back + UR) == 3
+    disp('Blanchard-Kahn Conditions are met !')
+end
+
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%                     	Saddle Path                			  %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -276,7 +272,6 @@ Paa = P1(3:5,3:5);
 
 % We split W into 2 systems of equation, W1 that depends on predetermined
 % and W2 that depends on jump such that W = [W1;W2]
-
 W1 = W(1:2,:);
 W2 = W(3:5,:);
 
@@ -289,27 +284,28 @@ W12 = W1(:,3:5);
 
 PIs = W11 - W12*inv(Paa)*Pas;
 
-% SADDLE PATH
+% We now want to express the forward variables as a linear combination of
+% shocks.
+
 mu_m = EIG(3);
 mu_mu = EIG(4);
 mu_l = EIG(5);
-% CL
 
+% CL
 C = P1*R*RHO+P1*Q;
 C = C(3:5,:);
 
-CL = [ -C(2,1)*1/(mu_m * rho_a)  -C(1,2)*1/(mu_m * rho_g);
-       -C(2,1)*1/(mu_mu * rho_a) -C(2,2)*1/(mu_mu * rho_g);
-       -C(3,1)*1/(mu_l * rho_a)  -C(3,2)*1/(mu_l * rho_g)];
+CL = [ -C(1,1)*1/(mu_m - rho_a)  -C(1,2)*1/(mu_m - rho_g);
+       -C(2,1)*1/(mu_mu - rho_a) -C(2,2)*1/(mu_mu - rho_g);
+       -C(3,1)*1/(mu_l - rho_a)  -C(3,2)*1/(mu_l - rho_g)];
 
 % Computation of PIa
-
 PIa = W12*inv(Paa)*CL+R(1:2,:)*RHO+Q(1:2,:);
 
-M = real([PIs PIa ; zeros(2) RHO]);
+M = [PIs PIa ; zeros(2) RHO];
 
 
-%% PI matrix:
+%% PId matrix:
 
 V = -inv(Paa)*Pas;
 U = inv(Paa)*CL;
@@ -324,90 +320,66 @@ PID(9,:) = lambda;
 
 X = inv(M1)*M2;
 Y = inv(M1)*M3;
-Z = X(:,3:5)*inv(Paa);
+Z = X(:,3:5)*inv(Paa)*Pas;
 
 G1 = X(:,1)-Z(:,1);
 G2 = X(:,2)-Z(:,2);
 G3 = X(:,3:5)*inv(Paa)*CL+Y;
 
 PID(1:7,:) = [G1 G2 G3];
-PID = real(PID);
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% IRF
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%      
+nrep = 10;
+
+%Monetary shock
+shock=[0;0;0;1];
+
+for i = 1:nrep+1
+    Rd(:,i) = (PID*M^(i-1))*shock;
+    Rstate(:,i) = (M^(i-1))*shock;
+end
+
+k = Rstate(1,1:nrep)';
+C = Rd(1,1:nrep)';
+H = Rd(2,1:nrep)';
+w = Rd(3,1:nrep)';
+z = Rd(4,2:(nrep+1))';
+Y = Rd(5,1:nrep)';
+pi = Rd(6,1:nrep)';
+f = Rd(7,1:nrep)';
+
+figure
+plot(1:nrep,[w,C,z],'-s');
+legend('w','C','r','Location','southoutside');
+title('French calibration - 1% shock on g')
+xlabel('Quarters');
+ylabel('relative deviation (%)');
+set(gcf, 'Color', [1,1,1]);
+hline = refline([0 0]);
+set(hline,'LineStyle','--','color','black');
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%M = [0.87 0.12 0.14 0.16; 0.36 0.05 0.47 -0.35; 0 0 rho_a 0; 0 0 0 rho_g];
-%PID = [0.33 0.05 0.5 0.04; -1.03 1.21 0.47 1.55;0.44 0.06 0.67 0.08;-1.59 1.27 1.14 1.63;-0.42 0.85 1.331 0.09;0.45 -1.3 2.29 -1.69;1.05 -2.57 1.14 -3.32;-0.44 -0.06 -0.67 -0.08;-0.36 0.95 -0.47 1.35];
-       
-periods = 20;
+%% Stochastic Simulation
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% 
 
-shock = [0 ; 0 ; 0 ; 1];
+nsimul = 5
 
-for i=1:periods;
-    RC=(M^(i-1))*shock;
-    RCK(i)=RC(1);
-    RCM(i)=RC(2);
-    RCA(i)=RC(3);
-    RCG(i)=RC(4);
-end;
+nlong=10;
 
-for i=1:periods;
-RC=(PID*M^(i-1))*shock;
- RCC(i)=RC(1);
- RCH(i)=RC(2);
- RCW(i)=RC(3);
- RCZ(i)=RC(4);
- RCY(i)=RC(5);
- RCP(i)=RC(6);
- RCF(i)=RC(7);
-end;
+% Drawing shocks
+aleaa = normrnd(0,epsilon_ap,nlong,nsimul);
+aleag = normrnd(0,epsilon_g,nlong,nsimul);
 
-figure
-subplot(221),plot(RCK(1:periods))
-title('Capital')
-xlabel('quarters')
-ylabel('% Dev.   ')
-
-subplot(223),plot(RCM(1:periods))
-title('Monnaie')
-xlabel('quarters')
-ylabel('% Dev.   ');
-
-figure
-subplot(221),plot(RCC(1:periods))
-title('Consumption')
-xlabel('quarters')
-ylabel('% Dev.   ')
-
-subplot(222),plot(RCH(1:periods))
-title('Hours')
-xlabel('quarters')
-ylabel('% Dev.   ')
-
-subplot(223),plot(RCW(1:periods))
-title('Wages')
-xlabel('quarters')
-ylabel('% Dev.   ');
-
-
-figure
-subplot(221),plot(RCZ(1:periods))
-title('Interest rate')
-xlabel('quarters')
-ylabel('% Dev.   ')
-
-subplot(222),plot(RCY(1:periods))
-title('Output')
-xlabel('quarters')
-ylabel('% Dev.   ')
-
-subplot(223),plot(RCP(1:periods))
-title('Profits')
-xlabel('quarters')
-ylabel('% Dev.   ')
-
-subplot(224),plot(RCF(1:periods))
-title('Inflation')
-xlabel('quarters')
-ylabel('% Dev.   ')
-
+% Drawing autoregressive shocks
+for i=2:nlong
+    epsa(i,:) = rho_ap*aleaa(i-1,:) + aleaa(i);
+    epsg(i,:) = rho_g*aleag(i-1,:) + aleag(i);
+end
+for j=2:nsimul
+    eps = [epsa(:,j) epsg(:,j);
+    MS(:,1)=[0;0];
+    for i=1:nlong
+        MS(:,1) = M(1:2,1:2)*MS(:,i-1)+M(1:2,3)*eps(i,1)+M(1:2,4)*eps(i,2);
+    end
+end
